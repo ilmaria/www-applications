cea65f5f3d02f8f2db0fab6583a160d229af7cfe
Ilmari Autio <ilmari.autio@gmail.com>
2016-11-05 22:25:01 +0200

    Add server side implementation for long polling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const EventEmitter = require('events').EventEmitter
 const express = require('express')
 const app = express()
 const WebSocketServer = require('ws').Server
@@ -6,13 +7,49 @@ const WebSocketServer = require('ws').Server
 const httpPort = 8000
 const wsPort = 8080
 
-let httpClients = []
+const longPollChannel = new EventEmitter()
+longPollChannel.setMaxListeners(Infinity)
 
+/**
+ * Interface - Chat message
+ * {
+ *   author: string
+ *   content: string
+ * }
+ */
+
+//----------------------------------
 // HTTP
+//----------------------------------
 app.use(express.static('client'))
 
 app.get('/', (req, res) => {
   res.render('client/index.html')
+})
+
+/**
+ * Clients use GET request to receive long poll messages. This blocks
+ * until `longPollChannel` receives a new message and then send it
+ * back to the client.
+ */
+app.get('/long-poll', (req, res) => {
+  longPollChannel.once('message', (message) => {
+    // send received message as json
+    res.json(message)
+  })
+})
+
+/**
+ * Clients use POST request for sending long poll messages.
+ */
+app.post('/long-poll', (req, res) => {
+  const message = req.body
+
+  console.log('Received a websocket message:\n-', message)
+
+  broadcast(message)
+  
+  res.status(200).end()
 })
 
 app.listen(httpPort, () => {
@@ -20,16 +57,34 @@ app.listen(httpPort, () => {
 })
 
 
+
+//----------------------------------
 // Websocket
+//----------------------------------
 const wsServer = new WebSocketServer({ port: wsPort })
 
 wsServer.on('connection', (ws) => {
   ws.on('message', (message) => {
-    console.log('Received a websocket message:\n-', JSON.parse(message))
+    const chatMessage = JSON.parse(message)
 
-    // broadcast message to all clients
-    for (const client of [...wsServer.clients, ...httpClients]) {
-      client.send(message)
-    }
+    console.log('Received a websocket message:\n-', chatMessage)
+
+    broadcast(chatMessage)
   })
 })
+
+/**
+ * Broadcast message to all clients
+ * @param {Object} message - Chat message
+ */
+function broadcast(message) {
+  // send message to websocket clients
+  for (const client of wsServer.clients) {
+    // websockets can only send string or blob data, so
+    // we need to turn javascript objects into string
+    client.send(JSON.stringify(message))
+  }
+
+  // send message to long poll clients
+  longPollChannel.emit('message', message)
+}
