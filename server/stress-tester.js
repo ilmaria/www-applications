3ecaba1903d569050d5c63e6bba1e854e9d3b587
Ilmari Autio <ilmari.autio@gmail.com>
2016-11-29 23:10:13 +0200

    Add benchmark timers to stress tester

--- server/stress-tester.js
+++ server/stress-tester.js
@@ -1,8 +1,8 @@
 const cluster = require('cluster')
 const numCPUs = require('os').cpus().length
-const virtualClient = require('./virtual-client')
+const VirtualClient = require('./virtual-client')
 
-const clientsPerCPUCore = 3
+const clientsPerCPUCore = 1
 
 if (cluster.isMaster) {
   // Fork workers.
@@ -10,15 +10,38 @@ if (cluster.isMaster) {
     cluster.fork()
   }
 
+  cluster.on('message', (worker, data) => {
+    console.log(`Ack time for worker ${worker.process.id}:`, data.avgAckTime)
+    console.log(`Server ack time for worker ${worker.process.id}:`, data.avgServerAckTime)
+  })
+
   cluster.on('exit', function(worker, code, signal) {
     console.log('worker ' + worker.process.pid + ' died')
   })
 } else {
+  let totalAckTime = 0
+  let totalServerAckTime = 0
+  let clientsDone = []
+
   for (let i = 0; i < clientsPerCPUCore; i++) {
     // Start a virtual client that sends and receives messages from the server.
-    // Clients have a random time when they start their connection.
-    setTimeout(function() {
-      virtualClient(cluster.worker.id * clientsPerCPUCore + i)
-    }, Math.random()* 3000)
+    const client = new VirtualClient(cluster.worker.id * clientsPerCPUCore + i)
+
+    let clientDone = client.waitUntilDone().then(({ackTime, serverAckTime}) => {
+      console.log('avg1')
+      totalAckTime += ackTime
+      totalServerAckTime += serverAckTime
+    })
+
+    clientsDone.push(clientDone)
   }
+
+  Promise.all(clientsDone).then(() => {
+    console.log('avg2')
+    // Send results to master
+    process.send({
+      avgAckTime: totalAckTime / clientsPerCPUCore,
+      avgServerAckTime: totalServerAckTime / clientsPerCPUCore
+    })
+  })
 }
--- server/virtual-client.js
+++ server/virtual-client.js
@@ -1,29 +1,72 @@
 const WebSocket = require('ws')
-
 const serverAddress = 'ws://localhost:8080'
 
-function virtualClient(id) {
-  const ws = new WebSocket(serverAddress)
+class VirtualClient {
+  constructor(id) {
+    this.id = id
+    this.timer = null
+    this.ackTime = 0
+    this.serverAckTime = 0
 
-  ws.on('open', () => {
+    // Connect to server after a timeout
+    this.ws = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(new WebSocket(serverAddress))
+      }, id * 5)
+    })
+  }
+
+  waitUntilDone() {
+    return this.ws.then((ws) => {
+      return new Promise((resolve, reject) => {
+        ws.on('open', () => {
+          // Wait until other clients have connected the server.
+          setTimeout(() => {
+            this.timer = process.hrtime()
+            this.sendMessage(ws, 'content 1')
+          }, 1000)
+        })
+
+        ws.on('message', (msg) => {
+          const message = JSON.parse(msg)
+
+          if (msg.messageType === "msg") {
+            this.sendAck(ws, msg.id)
+          } else if (msg.messageType === 'ack') {
+            const timeDiff = process.hrtime(this.timer)
+            // Time in microseconds
+            this.ackTime = timeDiff[0] * 1e6 + timeDiff[1] / 1e3
+            console.log('avg3')
+
+            resolve({
+              ackTime: this.ackTime,
+              serverAckTime: this.serverAckTime
+            })
+          } else if (msg.messageType === 'serverAck') {
+            console.log('avg4')
+            const timeDiff = process.hrtime(this.timer)
+            // Time in microseconds
+            this.serverAckTime = timeDiff[0] * 1e6 + timeDiff[1] / 1e3
+          }
+        })
+      })
+    })
+  }
+
+  sendMessage(ws, content) {
     ws.send(JSON.stringify({
-      author: 'user_' + id,
-      content: 'message content ' + id,
+      author: 'user_' + this.id,
+      content: content,
       messageType: 'msg'
     }))
-  })
+  }
 
-  ws.on('message', (msg) => {
-    const message = JSON.parse(msg)
-
-    if (msg.messageType === "msg") {
-      const response = {
-        id: msg.id,
-        messageType: "ack"
-      }
-      ws.send(JSON.stringify(response))
-    }
-  })
+  sendAck(ws, id) {
+    ws.send(JSON.stringify({
+      id: id,
+      messageType: "ack"
+    }))
+  }
 }
 
-module.exports = virtualClient
+module.exports = VirtualClient
